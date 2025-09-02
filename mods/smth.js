11bb935b40657eca26bdbf7875ea22742bd50c07
SuperShrew <150274640+SuperShrew@users.noreply.github.com>
2025-09-02 12:02:15 +0100

    Update smth.js

--- mods/smth.js
+++ mods/smth.js
@@ -1,3 +1,9 @@
+/*let conwayPhase = 0;
+
+function isInBounds(x, y) {
+    return x >= 0 && x < pixelMap.length && y >= 0 && y < pixelMap[0].length;
+}*/
+
 elements.mint = {
 	color: "#aaffaa",
 	behavior: behaviors.POWDER,
@@ -17,7 +23,7 @@ elements.linerx = {
 	category: "misc",
 	state: "solid",
 	tick: function(pixel) {
-        tryMove(pixelMap[pixel.x][pixel.y],pixel.x,pixel.y-1)
+        tryMove(pixel,pixel.x,pixel.y-1);
 	}
 }
 
@@ -27,24 +33,116 @@ elements.patternpowder = {
 	category: "misc",
 	state: "solid",
 	tick: function(pixel) {
-        tryMove(pixelMap[pixel.x][pixel.y],pixel.x-2,pixel.y-1)
-        tryMove(pixelMap[pixel.x][pixel.y],pixel.x+2,pixel.y)
+        tryMove(pixel,pixel.x-2,pixel.y-1);
+        tryMove(pixel,pixel.x+2,pixel.y);
 	}
 }
 
-elements.conway = {
-    //tbc
+/*elements.conway = {
+    color: "#00ff00",
+    category: "misc",
+    state: "solid",
+    tick: function(pixel) {
+        if (!conwayPhase) {
+            let nbrs = [
+                pixelMap[pixel.x+1]?.[pixel.y+1],
+                pixelMap[pixel.x+1]?.[pixel.y],
+                pixelMap[pixel.x+1]?.[pixel.y-1],
+                pixelMap[pixel.x]?.[pixel.y+1],
+                pixelMap[pixel.x]?.[pixel.y-1],
+                pixelMap[pixel.x-1]?.[pixel.y+1],
+                pixelMap[pixel.x-1]?.[pixel.y],
+                pixelMap[pixel.x-1]?.[pixel.y-1],
+            ];
+            let liveCells = 0;
+            for (let nbr of nbrs) {
+                if (nbr && nbr.element === "conway") {
+                    liveCells++;
+                }
+            }
+            pixel.newState = (liveCells == 2 || liveCells == 3) ? 1 : 0;
+        } else {
+            if (!pixel.newState) {
+                if (isInBounds(pixel.x, pixel.y)) {
+                    changePixel(pixel, "conwayDead");
+                }
+            }
+        }
+    },
+    onChange: function(pixel, newElement) {
+        // do nothing
+    },
 }
+
+elements.conwayDead = {
+    color: "#003300",
+    category: "misc",
+	state: "solid",
+	density: 0,
+	hardness: 0,
+	behavior: behaviors.STILL,
+    tick: function(pixel) {
+        if (!conwayPhase) {
+            let nbrs = [
+                pixelMap[pixel.x+1]?.[pixel.y+1],
+                pixelMap[pixel.x+1]?.[pixel.y],
+                pixelMap[pixel.x+1]?.[pixel.y-1],
+                pixelMap[pixel.x]?.[pixel.y+1],
+                pixelMap[pixel.x]?.[pixel.y-1],
+                pixelMap[pixel.x-1]?.[pixel.y+1],
+                pixelMap[pixel.x-1]?.[pixel.y],
+                pixelMap[pixel.x-1]?.[pixel.y-1],
+            ];
+            let liveCells = 0;
+            for (let nbr of nbrs) {
+                if (nbr && nbr.element === "conway") {
+                    liveCells++;
+                }
+            }
+            pixel.beAlive = (liveCells === 3);
+        } else {
+            if (pixel.beAlive) {
+                if (isInBounds(pixel.x, pixel.y)) {
+                    changePixel(pixel, "conway");
+                }
+            }
+        }
+    },
+    onChange: function(pixel, newElement) {
+        // do nothing
+    },
+}
+
+elements.conwayManager = {
+    color: "#ff00ff",
+    category: "misc",
+    state: "solid",
+    tick: function(pixel) {
+        conwayPhase = 1 - conwayPhase;  // toggle 0/1 each tick
+    }
+}*/
 
 elements.bore = {
     color: "#aaaaaa",
     behavior: [
         ["XX", "XX", "XX"],
-        ["XX", "XX", "XX"],
-        ["XX", "XX", "XX"]
-    ]
+        ["DL", "XX", "DL"],
+        ["DL", "DL AND M1", "DL"]
+    ],
+    category: "misc",
+    state: "solid",
 }
 
-elements.conway = {
-    //tbc
+elements.irratic = {
+    color: "#ffaa00",
+    behavior: [
+        ["M1", "M1", "M1"],
+        ["M1", "XX", "M1"],
+        ["M1", "M1", "M1"]
+    ],
+    category: "misc",
+    state: "solid",
+    reactions: {
+        "linerx": {elem1: "nuke", elem2: "nuke"}
+    }
 }
